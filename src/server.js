1c0a2c9a535ea0cbe437f2f3ae6388b4fc4f3896
juelsonjunior <juelsonjunior27@gmail.com>
2025-06-22 04:09:10 +0100

    feat:starting server with express

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import dotenv from "dotenv";
+import Routes from "./routes/index.js";
+import connectBd from "./database/bd.js";
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(Routes);
+
+connectBd
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`Servidor rodando na porta http://localhost:${port}`)
+    );
+  })
+  .catch((err) => {
+    console.log("Falha ao conectar com o banco de dados", err);
+  });
