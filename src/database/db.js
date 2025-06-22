62e0079a805480f75d66e2e5d0fb99a64ddda719
juelsonjunior <juelsonjunior27@gmail.com>
2025-06-22 03:57:46 +0100

    feat:implement conection with data base

---
+++ src/database/db.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+async function connectBd() {
+  await mongoose.connect(process.env.DB_HOST);
+}
+
+export default connectBd
