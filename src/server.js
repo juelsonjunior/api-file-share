bf63d1f82d8aa9d92775597fc11a73312331b3a4
juelsonjunior <juelsonjunior27@gmail.com>
2025-06-22 04:31:16 +0100

    fix:resolve problem of import bd

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import Routes from "./routes/index.js";
-import connectBd from "./database/bd.js";
+import connectBd from "./database/db.js";
 
 dotenv.config();
 const app = express();
