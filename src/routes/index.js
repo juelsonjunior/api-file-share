2b9f086082b55856910406b43f2d10b459f325fa
juelsonjunior <juelsonjunior27@gmail.com>
2025-06-23 03:03:19 +0100

    feat:add middleware in routes privates

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import User from "./user.js";
 import File from "./file.js";
+import { authenticateUser } from "../middleware/authUser.js";
 
 const routes = express();
 
 routes.use("/", User);
-routes.use("/", File);
+routes.use("/", authenticateUser, File);
 
 export default routes;
